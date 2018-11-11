d85efd7b79f0244dbdea66a88e73f4486148fc8f
Gouthami Reddy <Gouthami Reddy@Roomy.home>
2018-11-10 22:13:02 -0500

    first commit

---
+++ com.testing/src/main/java/com/gowthami/com/testing/App.java
@@ -0,0 +1,13 @@
+package com.gowthami.com.testing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
